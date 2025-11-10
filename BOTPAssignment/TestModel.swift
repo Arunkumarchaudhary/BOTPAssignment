1061f86942064dfc95619f8c2056300e34768da8
Arun Kumar Chaudhary <arunkumarchaudhary@Aruns-MacBook-Air.local>
2025-11-10 15:10:52 +0530

    unit initialtteadted

---
+++ BOTPAssignment/TestModel.swift
@@ -0,0 +1,18 @@
+//
+//  TestModel.swift
+//  BOTPAssignment
+//
+//  Created by Arun Kumar Chaudhary on 10/11/25.
+//
+
+import SwiftUI
+
+struct TestModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestModel()
+}
